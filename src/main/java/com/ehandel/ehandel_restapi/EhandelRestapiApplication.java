98beeda2429a51be55e4cc82a4ce3a4c34b7006b
Arif Hossain <hossaininfo@ymail.com>
2024-03-13 15:19:49 +0100

    Initial commit

---
+++ src/main/java/com/ehandel/ehandel_restapi/EhandelRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.ehandel.ehandel_restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhandelRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhandelRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ehandel/ehandel_restapi/EhandelRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehandel.ehandel_restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhandelRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
